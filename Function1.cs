da91fbb366680243ecc156131ad2c9c708d1f74f
Curtis Coker III <E410521@exelonds.com>
2020-06-12 12:35:37 -0400

    changed the function

--- Function1.cs
+++ Function1.cs
@@ -26,7 +26,7 @@ namespace Orianhttpcalculator
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "Lif is a highway. This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
